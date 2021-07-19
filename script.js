03dfbd3ebdd40df9f7d8d9ca1d937d828fe59d39
Laysa Souza <karollinylaysa@gmail.com>
2021-07-19 21:03:27 +0100

    Added icons for the night - API (03n...50n)

--- script.js
+++ script.js
@@ -2,9 +2,19 @@
 const apiKey = "718938e8a3e4822470de1037fd72347a";
 let celsiusTemperature = null;
 let currentFeelsLike = null;
+let currentMaxTemperature = null;
+let currentMinTemperature = null;
 const temperatureElement = document.querySelector("#temperature");
 const feelsLikeElement = document.querySelector("#feels-like");
 const maxMinTemperatureElement = document.querySelector("#high-low-temp");
+const cityElement = document.querySelector("#city");
+const DescriptionElement = document.querySelector("#weather-description");
+const humidityElement = document.querySelector("#humidity");
+const visibilityElement = document.querySelector("#visibility");
+const windElement = document.querySelector("#wind");
+const dateTimeElement = document.querySelector("#date-time");
+const iconElement = document.querySelector("#icon");
+
 
 function formatDate(date) {
 //calculate the date
@@ -28,32 +38,17 @@ function formatDate(date) {
 }
 
 function displayTemperature(response) {
-    const cityElement = document.querySelector("#city");
+
     let currentCity = response.data.name;
     let currentCountry = response.data.sys.country;
     celsiusTemperature = Math.round(response.data.main.temp);
-
-    const DescriptionElement = document.querySelector("#weather-description");
     let currentDescription = response.data.weather[0].description;
-    const humidityElement = document.querySelector("#humidity");
     let currentHumidity = response.data.main.humidity;
-    const visibilityElement = document.querySelector("#visibility");
     let currentVisibility = (response.data.visibility) / 1000;
-
     currentFeelsLike = Math.round(response.data.main.feels_like);
-    const windElement = document.querySelector("#wind");
     let currentWind = Math.round(response.data.wind.speed);
-
-    let currentMaxTemperature = Math.round(response.data.main.temp_max);
-    let currentMinTemperature = Math.round(response.data.main.temp_min);
-    const dateTimeElement = document.querySelector("#date-time");
-    const iconElement = document.querySelector("#icon");
-
-    // Applying Timezone
-    let utcTime = new Date();
-    let localTime = new Date(utcTime.getTime() + response.data.timezone * 1000);
-    dateTimeElement.innerHTML = formatDate(localTime);
-
+    currentMaxTemperature = Math.round(response.data.main.temp_max);
+    currentMinTemperature = Math.round(response.data.main.temp_min);
     cityElement.innerHTML = `${currentCity}, ${currentCountry}`;
     temperatureElement.innerHTML = `${celsiusTemperature}°`;
     DescriptionElement.innerHTML = currentDescription;
@@ -63,24 +58,37 @@ function displayTemperature(response) {
     windElement.innerHTML = `<strong>${currentWind}km/H</strong>`;
     maxMinTemperatureElement.innerHTML = `<i class="fas fa-arrow-circle-up"></i> Day ${currentMaxTemperature}° <i class="fas fa-arrow-circle-down"></i>
     Night ${currentMinTemperature}°`;
-
+    // Applying Timezone
+    let utcTime = new Date();
+    let localTime = new Date(utcTime.getTime() + response.data.timezone * 1000);
+    dateTimeElement.innerHTML = formatDate(localTime);
+    // Change Icons
     const iconMap = {
         '01d': 'sun.png',
         '01n': 'night.png',
         '02d': 'cloudy.png',
         '02n': 'cloud-night',
         '03d': 'cloud.png',
+        '03n': 'cloud.png',
         '04d': 'broken-clouds.png',
+        '04n':'broken-clouds.png',
         '09d': 'rain.png',
+        '09n': 'rain.png',
         '10d': 'rainy.png',
+        '10n': 'rainy.png',
         '11d': 'storm.png',
+        '11n': 'storm.png',
         '13d': 'snowy.png',
+        '13n': 'snowy.png',
         '50d': 'mist.png',
+        '50n': 'mist.png',
     };
 
     let iconApi = response.data.weather[0].icon;
+    console.log(iconApi)
     iconElement.setAttribute("src", `images/${iconMap[iconApi]}`); //Changing attribute "src" to another value
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
 
 }
 
@@ -92,7 +100,7 @@ function updateCity(cityName) {
 
 function handleSubmit(event) {
     event.preventDefault();
-    const searchInput = inputSearchElement.value.trim();
+    let searchInput = inputSearchElement.value.trim();
     updateCity(searchInput);
 
 }
@@ -131,6 +139,11 @@ function displayFahrenheitTemperature(event) {
     temperatureElement.innerHTML = `${fahrenheitTemperature}°`;
     currentFeelsLike = Math.round((currentFeelsLike * 9) / 5 + 32);
     feelsLikeElement.innerHTML = `<strong>${currentFeelsLike}°</strong>`;
+    //currentMaxTemperature = Math.round((currentMaxTemperature * 9) / 5 + 32);
+   // currentMinTemperature = Math.round((currentMinTemperature * 9) / 5 + 32);
+    //maxMinTemperatureElement.innerHTML = `<i class="fas fa-arrow-circle-up"></i> Day ${currentMaxTemperature}° <i class="fas fa-arrow-circle-down"></i>
+    //Night ${currentMinTemperature}°`;
+    console.log(currentMaxTemperature, currentMinTemperature)
 }
 
 function displayCelsiusTemperature(event) {
@@ -138,9 +151,9 @@ function displayCelsiusTemperature(event) {
     fahrenheitButton.classList.remove("disabled")
     celsiusButton.classList.add("disabled");
     temperatureElement.innerHTML = `${celsiusTemperature}°`;
-   currentFeelsLike = Math.round((currentFeelsLike -32) *5/9);
-   feelsLikeElement.innerHTML = `<strong>${currentFeelsLike}°</strong>`;
-   console.log(currentFeelsLike)
+    currentFeelsLike = Math.round((currentFeelsLike -32) *5/9);
+    feelsLikeElement.innerHTML = `<strong>${currentFeelsLike}°</strong>`;
+    console.log(currentFeelsLike)
 }
 
 // Events listener
